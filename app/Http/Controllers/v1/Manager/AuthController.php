6b172a9a0defaf99f01726c0dc8fd9ebd973bea1
Renan Joppert <renanjoppert@outlook.com>
2021-03-16 00:37:00 -0300

    Tradução das mensagens do AuthController

--- app/Http/Controllers/v1/Manager/AuthController.php
+++ app/Http/Controllers/v1/Manager/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Sem Autorização'], 401);
         }
 
         return $this->respondWithToken($token);
@@ -52,7 +52,7 @@ class AuthController extends Controller
     {
         auth()->logout();
 
-        return response()->json(['message' => 'Successfully logged out']);
+        return response()->json(['message' => 'Logout realizado com sucesso']);
     }
 
     /**
